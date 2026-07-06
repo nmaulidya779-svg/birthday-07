10be962a655c6a10b88e146af3f2d0f26355deab
nmaulidya779-svg <nmaulidya779@gmail.com>
2026-07-06 19:35:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+const CONFIG = {
+
+    nama: "Naufal",
+
+    music: "music/birthday.mp3",
+
+    video: "video/adadehh.mp4",
+
+    intro: "ini aku buat khusus untuk kamu🤍",
+
+    opening: "Hai, selamat datang. Sebelum kamu melanjutkan perjalanan ini, aku cuma ingin bilang kalau semua yang ada di sini dibuat khusus untuk kamu. Mungkin sederhana, tapi setiap kata, setiap foto, dan setiap kenangan di dalamnya dipilih dengan penuh ketulusan. Selamat menikmati kejutan kecil ini. 🤍",
+
+    letter: [
+        "Selamat ulang tahun ya, botak tak tak wkwkw! Sumpah yaa, aku bersyukur banget bisa kenal dan punya teman sebaik kamu. Nggak semua orang beruntung bisa ketemu temen yang tulus kaya kamu, dan aku beruntung banget karena orang itu adalah kamu.",
+        "Makasih banyak ya udah selalu ada di segala situasi, bahkan di saat-saat paling random sekalipun. Kamu itu tipe teman yang nggak pernah absen kalau dimintain tolong, selalu siap dengerin curhatanku yang panjang lebar, dan yang paling juara kamu tuh sabar banget ngadepin semua tingkah aku.",
+        "Aslinya aku kadang gengsi mau ngomong gini, tapi kamu itu beneran cowok paling baik yang pernah aku kenal. Kebaikanmu itu alami banget, nggak dibuat-buat, dan selalu bisa bikin orang-orang di sekitar kamu merasa nyaman dan dihargai,termasuk aku.",
+        "Di umur yang baru ini, doa terbaikku bakal selalu menyertai setiap langkahmu. Semoga semua impian besar, rencana-rencana keren, dan cita-cita yang sering kamu ceritain ke aku bisa segera terwujud satu per satu dalam waktu dekat.",
+        "Aku juga berdoa supaya kamu selalu dikasih kesehatan, kebahagiaan yang nggak habis-habis, dan rezeki yang makin lancar biar bisa terus jadi berkah buat orang lain. Makin dewasa, makin matang, dan pokoknya makin sukses dalam karier atau apa pun yang lagi kamu kejar sekarang.",
+        "Tetap jadi sosok yang asyik, humble, dan baik hati kayak yang aku kenal sekarang, ya!"
+    ],
+
+    gallery: [
+        "images/foto2.jpg",
+        "images/foto3.jpg",
+        "images/foto4.jpg"
+    ],
+
+    timeline: [
+        {
+            year: "Awal",
+            text: "Awal pertama kali kita bertemu."
+        },
+        {
+            year: "Banyak Cerita",
+            text: "Banyak momen indah yang sudah kita lewati bersama."
+        },
+        {
+            year: "Hari Ini",
+            text: "Selamat ulang tahun 🤍 Semoga semua impianmu tercapai."
+        }
+    ]
+
+};
